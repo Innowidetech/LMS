32298246a709ba1ad59e0c20fc15347cab11dbfb
Innowidetech <innowidetech@gmail.com>
2024-11-18 10:49:17 +0530

    dependencies

--- server/@types/custom.d.ts
+++ server/@types/custom.d.ts
@@ -8,33 +8,4 @@ declare global{
         }
 
     }
-};
-
-// Module declaration for `express-rate-limit` (if types are not found)
-declare module 'express-rate-limit' {
-    import { RequestHandler } from 'express';
-
-    interface RateLimitOptions {
-        windowMs: number;
-        limit: number;
-        standardHeaders?: string;
-        legacyHeaders?: boolean;
-    }
-
-    function rateLimit(options: RateLimitOptions): RequestHandler;
-
-    export = rateLimit;
-};
-
-// Module declaration for `axios` (optional, if types are not found)
-declare module 'axios' {
-    import { AxiosInstance } from 'axios';
-    const axios: AxiosInstance;
-    export default axios;
-};
-
-// Module declaration for `socket.io` (optional, if types are not found)
-declare module 'socket.io' {
-    import { Server as SocketIOServer } from 'socket.io';
-    export = Server;
-};
+}
